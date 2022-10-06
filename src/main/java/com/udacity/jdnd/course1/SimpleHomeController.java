abcc1a32664c3e04be8bb9162a98aa25b37badb9
Felix <busayofelix@gmail.com>
2022-10-06 06:05:42 +0100

    no message

--- src/main/java/com/udacity/jdnd/course1/SimpleHomeController.java
+++ src/main/java/com/udacity/jdnd/course1/SimpleHomeController.java
@@ -1,6 +1,19 @@
 package com.udacity.jdnd.course1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SimpleHomeController {
 
     private boolean firstMessage;
+
+   @GetMapping("/simple-home")
+    public String getHomePage() {
+        //model.addAttribute("greetings", new String[] { "Hi", "Hello", "Goodbye"});
+        return "simple-home";
+    }
+
 }
